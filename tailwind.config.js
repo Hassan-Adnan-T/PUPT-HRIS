1110ba1e16d86f43e25fc0a3fb4728f6d881f941
hassan_adnan <adnanhassanda@gmail.com>
2024-07-21 17:54:03 +0800

    make it lightmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
-export default {
+module.exports = {
   content: [
-    // You will probably also need those lines
     "./resources/**/**/*.{js,blade.php}",
     "./app/View/Components/**/**/*.php",
     "./app/Livewire/**/**/*.php",
-
-    // Add mary
     "./vendor/robsontenorio/mary/src/View/Components/**/*.php",
   ],
-
-  // Add daisyUI
-  plugins: [require("daisyui")],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui"),
+  ],
+  daisyui: {
+    themes: ["light"],
+  },
 }
